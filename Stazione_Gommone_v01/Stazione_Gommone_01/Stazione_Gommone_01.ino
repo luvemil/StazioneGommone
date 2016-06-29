1c4ac1dc27abdef07c4a2c8a4d1abced864c95ab
pr3sailing <pr3sailing@gmail.com>
2016-06-29 18:47:46 +0200

    Reads the data and sends them via i2c

--- Stazione_Gommone_v01/Stazione_Gommone_01/Stazione_Gommone_01.ino
+++ Stazione_Gommone_v01/Stazione_Gommone_01/Stazione_Gommone_01.ino
@@ -12,34 +12,46 @@ Dott. Marco Tarantino
 
 
 #define WSPIN 2
+#define LEDPIN  8
 #define ADD 0x25
 byte data[4];
 
-int wd;
-byte ws, bat;
+int wd = 920;
+byte ws = 200;
+byte bat = 100;
 
 void setup() {
 Wire.begin(ADD);
+Serial.begin(57600);
 Wire.onRequest(requestEvent);
 
 pinMode(WSPIN,INPUT_PULLUP);
-attachInterrupt(digitalPinToInterrupt(WSPIN), ISR_FUNC, RISING);
+pinMode(LEDPIN,OUTPUT);
+attachInterrupt(digitalPinToInterrupt(WSPIN), ISR_FUNC, FALLING);
+  digitalWrite(LEDPIN,HIGH);
+  delay(1000);
+  digitalWrite(LEDPIN,LOW);
 
 }
 void loop() {
   wd = analogRead(A0);
-  bat=analogRead(A1);
+  //Serial.println(wd);
+  //bat= analogRead(A1);
   data[0]=wd; //LSB wd
   data[1]=wd>>8; //MSB wd
+  data[2]=ws;
+  data[3]=bat;
+  delay(500);
   
 
 }
 
 void requestEvent(){
+  digitalWrite(LEDPIN,HIGH);
   Wire.write(data,4);
   ws=0;
+  digitalWrite(LEDPIN,LOW);
 }
-
 void ISR_FUNC(){
   ++ws;
 }
